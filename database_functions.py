a42529533938067196d249da5e635d272319cf2a
Luís Otávio Amorim <luisotaviolamorim@gmail.com>
2020-06-02 16:42:53 -0300

    Added functions to connect and disconnect from the database

--- database_functions.py
+++ database_functions.py
@@ -1,8 +1,16 @@
 import sqlite3
 
-def create_database():
+def connect():
     conn = sqlite3.connect('passwords.db')
     cursor = conn.cursor()
+    return conn, cursor
+
+def disconnect(conn):
+    conn.commit()
+    conn.close()
+
+def create_database():
+    conn, cursor = connect()
 
     cursor.execute('''
         CREATE TABLE "users" (
@@ -25,101 +33,82 @@ def create_database():
         );
     ''')
 
-    conn.commit()
-    conn.close()
+    disconnect(conn)
 
 def get_usernames_list():
-    conn = sqlite3.connect('passwords.db')
-    cursor = conn.cursor()
+    conn, cursor = connect()
 
     cursor.execute('SELECT username FROM users')
     users_list = cursor.fetchall()
 
-    conn.commit()
-    conn.close()
+    disconnect(conn)
 
     return users_list
 
 def add_user(username, master_hashed, key):
-
-    conn = sqlite3.connect('passwords.db')
-    cursor = conn.cursor()
+    conn, cursor = connect()
 
     cursor.execute(f'''INSERT INTO users ("username", "master_password", "key")
                     VALUES ("{username}", "{master_hashed}", "{key}")''')
 
-    conn.commit()
-    conn.close()
+    disconnect(conn)
 
 def get_master_hashed(username):
-    conn = sqlite3.connect('passwords.db')
-    cursor = conn.cursor()
+    conn, cursor = connect()
 
     cursor.execute(f'SELECT master_password FROM users WHERE username="{username}"')
     master_password_hashed = cursor.fetchone()
 
-    conn.commit()
-    conn.close()
+    disconnect(conn)
 
     return master_password_hashed[0]
 
 def get_user_id(username):
-    conn = sqlite3.connect('passwords.db')
-    cursor = conn.cursor()
+    conn, cursor = connect()
 
     cursor.execute(f'SELECT id FROM USERS WHERE username="{username}"')
     user_id = cursor.fetchone()[0]
+    disconnect(conn)
+
     return user_id
 
 def delete_user(user_id):
-    conn = sqlite3.connect('passwords.db')
-    cursor = conn.cursor()
+    conn, cursor = connect()
 
     cursor.execute(f'DELETE FROM users WHERE id={user_id}')
     cursor.execute(f'DELETE FROM services WHERE user_id={user_id}')
 
-    conn.commit()
-    conn.close()
+    disconnect(conn)
 
 def list_saved_services(user_id):
-    conn = sqlite3.connect('passwords.db')
-    cursor = conn.cursor()
+    conn, cursor = connect()
 
     cursor.execute(f'SELECT service_name FROM services WHERE user_id="{user_id}"')
     services = cursor.fetchall()
-
-    conn.commit()
-    conn.close()
+    disconnect(conn)
 
     return services
 
 def get_key(user_id):
-    conn = sqlite3.connect('passwords.db')
-    cursor = conn.cursor()
+    conn, cursor = connect()
 
     cursor.execute(f'SELECT key FROM users where id="{user_id}"')
     key = cursor.fetchone()[0]
     key = key[2:-1]
-    
-    conn.commit()
-    conn.close()
+    disconnect(conn)
 
     return key.encode()
 
 def add_service(service_name, username, encrypted_password, user_id):
-    conn = sqlite3.connect('passwords.db')
-    cursor = conn.cursor()
+    conn, cursor = connect()
 
     cursor.execute(f'''
                     INSERT INTO services (service_name, username, password, user_id)
                     VALUES ("{service_name}", "{username}", "{encrypted_password}", "{user_id}")''')
-    
-    conn.commit()
-    conn.close()
+    disconnect(conn)
 
 def check_data_from_service(user_id, service_name):
-    conn = sqlite3.connect('passwords.db')
-    cursor = conn.cursor()
+    conn, cursor = connect()
 
     cursor.execute(f'''
                     SELECT username, password
@@ -129,42 +118,32 @@ def check_data_from_service(user_id, service_name):
                     ''')
 
     results = cursor.fetchone()
-    conn.commit()
-    conn.close()
+    disconnect(conn)
 
     return results[0], results[1][2:-1]
 
 def update_service_username(user_id, service, username):
-    conn = sqlite3.connect('passwords.db')
-    cursor = conn.cursor()
+    conn, cursor = connect()
     
     cursor.execute(f'''
                     UPDATE services
                     SET username="{username}"
                     WHERE user_id="{user_id}"
                     AND service_name="{service}"''')
-    
-    conn.commit()
-    conn.close()
+    disconnect(conn)
 
 def update_service_password(user_id, service, password):
-    conn = sqlite3.connect('passwords.db')
-    cursor = conn.cursor()
+    conn, cursor = connect()
     
     cursor.execute(f'''
                     UPDATE services
                     SET password="{password}"
                     WHERE user_id="{user_id}"
                     AND service_name="{service}"''')
-    
-    conn.commit()
-    conn.close()
+    disconnect(conn)
 
 def delete_service(user_id, service):
-    conn = sqlite3.connect('passwords.db')
-    cursor = conn.cursor()
+    conn, cursor = connect()
 
     cursor.execute(f'DELETE FROM services WHERE user_id="{user_id}" AND service_name="{service}"')
-
-    conn.commit()
-    conn.close()
+    disconnect(conn)
